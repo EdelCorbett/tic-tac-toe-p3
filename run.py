8d3e5deb5bf4ed117c8e49473711793655f616a3
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2023-08-22 23:05:35 +0100

    Create game board

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
 import random
+import colorama
 from colorama import Fore, Back, Style
+
+print("TIC TAC TOE")
+
+# Create a board
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("---------")
+
+def game_board( ):
+    return [["1", "2", "3"], 
+            ["4", "5", "6"], 
+            ["7", "8", "9"]]
+board = game_board()
+
+print_board(board)
 
