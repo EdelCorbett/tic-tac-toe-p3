bb22db5e4880538930d7b7e83643887f3e28893f
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2023-08-23 18:30:07 +0100

    installed simple-term-menu

--- run.py
+++ run.py
@@ -8,8 +8,11 @@ print("TIC TAC TOE")
 def print_board(board):
     border = f"{Fore.RED}---------{Fore.RESET}"
     separator = f"{Fore.RED} | {Fore.RESET}"
-
+# changes the color of num to color of X or O
     for row in board:
+        colored_row = [cell if cell in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+                    else (f"{Fore.YELLOW}{cell}{Fore.RESET}" if cell == "X"
+                            else f"{Fore.BLUE}{cell}{Fore.RESET}") for cell in row]
         print(separator.join(row))
         print(border)
 
