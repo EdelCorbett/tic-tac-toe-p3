b5231c959718e5b6be36935aaa74b09b44634c74
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2023-08-24 14:51:15 +0100

    Add comments for functions

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
 import random
 import time
-import colorama
 from colorama import Fore
 from simple_term_menu import TerminalMenu
-
-
 
 def game_board(board):
     border = f"{Fore.RED}---------{Fore.RESET}"
@@ -17,36 +14,45 @@ def game_board(board):
         print(separator.join(colored_row))
         print(border)
 
-
+# This fuction create a new game board
+# Numbers the cells from 1 to 9
 def start_game_board():
     return [["1", "2", "3"],
             ["4", "5", "6"],
             ["7", "8", "9"]]
-
+# This function checks if the player has won
+# by checking all the possible winning combinations
 def check_for_win(board, player):
+    #checks rows
     for row in board:
         if all(cell == player for cell in row):
             return True
-
+# checks columns
     for col in range(3):
         if all(board[row][col] == player for row in range(3)):
             return True
-
+# checks diagonals
     if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
         return True
-
+# if none of the above is true, return false 
     return False
-
+# This function checks if the game is a tie
 def check_for_tie(board):
+    # if all cells are full and no one has won it's a tie
     return all(cell in ["X", "O"] for row in board for cell in row)
-
+# this function checks for empty cells and returns a random one
+# for the computers move and returns a random choice
 def computer_move(board):
     empty_cells = [(row, col) for row in range(3) for col in range(3) if board[row][col] not in ["X", "O"]]
     return random.choice(empty_cells) if empty_cells else None
-
+# This function gets the player to input their name
 def get_player_name():
     return input("Enter your name (X): ")
-
+"""
+ this function chooses how the game will be played
+    either player vs player or player vs computer
+    and returns the choice
+"""
 def choose_players():
     player_options = ["Player vs. Player", "Player vs. Computer"]
     terminal_menu = TerminalMenu(player_options, title="Tic Tac Toe - Choose Players")
@@ -55,7 +61,12 @@ def choose_players():
         return "Player vs. Player"
     else:
         return "Player vs. Computer"
-
+"""
+this function starts the game
+it get player to enter their names
+tell them their symbols
+and starts the game
+start the game with player X"""
 def play_game():
     game_mode = choose_players()
     
@@ -68,7 +79,7 @@ def play_game():
     
     board = start_game_board()
     current_player = "X"
-
+# This is the game loop
     while True:
         game_board(board)
 
@@ -108,15 +119,19 @@ def play_game():
             game_board(board)
             print("It's a draw!")
             break
-
+# changes player after each move
         current_player = "O" if current_player == "X" else "X"
-
+""""
+this function asks the player if they want to play again
+if yes, it starts the game again
+if no, it ends the game
+"""
     play_again = input("Play again (y/n)? ")
     if play_again.lower() == "y":
         play_game()
     else:
         print("Goodbye!")
     
-
+# This is the main function that runs the game
 if __name__ == "__main__":
     play_game()
