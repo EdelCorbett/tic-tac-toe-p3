d93ab4eca9ed1db1db4239cccf7d402d4fb0df73
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2023-08-28 11:39:01 +0100

    Change if statment fot q input

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 import random
 import time
-from colorama import Fore,Style
+from colorama import Fore, Style
 from simple_term_menu import TerminalMenu
 
 PLAYER_COLORS = {"X": Fore.LIGHTYELLOW_EX, "O": Fore.LIGHTBLUE_EX}
 
 
 def game_board(board):
+    """""This function displays the game board
+    in color using colorama
+    and colors the players symbols
+    """
     border = f"{Fore.RED}---------{Fore.RESET}"
     separator = f"{Fore.RED} | {Fore.RESET}"
 
@@ -14,22 +18,26 @@ def game_board(board):
         colored_row = [
             cell if cell in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
             else (f"{Fore.LIGHTYELLOW_EX}{cell}{Fore.RESET}" if cell == "X"
-                else f"{Fore.LIGHTBLUE_EX}{cell}{Fore.RESET}") for cell in row]
-        
+            else f"{Fore.LIGHTBLUE_EX}{cell}{Fore.RESET}") for cell in row]
         print(separator.join(colored_row))
         print(border)
 
 
-def start_game_board():
-    return [["1", "2", "3"],
-            ["4", "5", "6"],
-            ["7", "8", "9"]]
+def start_game_board() :
+    """This function creates the game board in grid format"""
+    return [
+        ["1", "2", "3"],
+        ["4", "5", "6"],
+        ["7", "8", "9"]
+        ]
+
 
 def show_rules():
     """This function displays the rules of the game
     and gives a 1 second delay between each rule
     for easy readability
     """
+
     rules = [
         " THE RULES OF TIC-TAC-TOE",
         " This game is played on a 9 space board ",
@@ -41,24 +49,27 @@ def show_rules():
         " To Play again press 'y'",
         " Good Luck!"
     ]
+
     for rule in rules:
         print(rule)
         time.sleep(1)
+
 
 def check_for_win(board, player):
     """Checks if the player has won
     by checking all the possible winning combinations
     checks rows columns and diagonals
     """
+
     for row in board:
         if all(cell == player for cell in row):
             return True
 
-
     for col in range(3):
         if all(board[row][col] == player for row in range(3)):
             return True
-        if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        if all(board[i][i] == player for i in range(3)
+            ) or all(board[i][2 - i] == player for i in range(3)):
             return True 
     return False
 
@@ -73,7 +84,8 @@ def computer_move(board):
     it checks if cell is empty
     if empty it returns a random cell
     """
-    empty_cells = [(row, col) for row in range(3) for col in range(3) if board[row][col] not in ["X", "O"]]
+    empty_cells = [(row, col) for row in range(3
+                ) for col in range(3) if board[row][col] not in ["X", "O"]]
     return random.choice(empty_cells) if empty_cells else None
 
 
@@ -105,7 +117,8 @@ def choose_players():
     from a terminal menu
     """
     player_options = ["Player vs. Player", "Player vs. Computer", "Show Rules"]
-    terminal_menu = TerminalMenu(player_options, title="Tic Tac Toe - Choose Players")
+    terminal_menu = TerminalMenu(
+        player_options, title="Tic Tac Toe - Choose Players")
     selected_index = terminal_menu.show()
 
     if selected_index == 0:
@@ -114,13 +127,14 @@ def choose_players():
         return "Player vs. Computer"
     elif selected_index == 2:
         show_rules()
-        input(f"{Fore.LIGHTMAGENTA_EX}Press Enter to continue.{Style.RESET_ALL}")
+        input(
+            f"{Fore.LIGHTMAGENTA_EX}Press Enter to continue.{Style.RESET_ALL}")
         return choose_players()
     else:
         print(f"{Fore.GREEN}Goodbye!{Fore.RESET}")
         return None
 
-    
+
 def play_game():
     """This is the main function that runs the game
     it get game mode and player names
@@ -167,14 +181,29 @@ def play_game():
                 )
 
             if position.lower() == 'q':
-                print(f"{Fore.GREEN}Goodbye!{Style.RESET_ALL}")
-                input(f"{Fore.LIGHTMAGENTA_EX}Press Enter to Return to Game Menu.{Style.RESET_ALL}")
-                break
-                    
+                confirm = input("Are you sure you want to quit (y/n)?")
+                if confirm == "y":
+                    print(f"{Fore.GREEN}Goodbye!{Style.RESET_ALL}")
+                    return choose_players()
+                else:
+                    continue
+            
+            """
+            Checks if input is a number
+            and between 1 and 9
+            if not it asks for input again
+            """       
             if not position.isdigit() or not (1 <= int(position) <= 9):
                 print(f"{Fore.RED}Invalid input. Please enter a number between 1 and 9.{Style.RESET_ALL}")
                 continue
-
+            """
+            Int is making sure the input is a number 
+            Divmod is used to get the row and column
+            from the position entered by the player
+            -1 is used to get the correct index
+            Then checks if the position is already occupied
+            if occupied it asks for input again
+            """
             position = int(position) - 1
             row, col = divmod(position, 3)
 
@@ -193,7 +222,7 @@ def play_game():
             if game_mode == "Player vs. Computer" and current_player == "O":
                 print(f"{Fore.LIGHTBLUE_EX}Computer wins!{Style.RESET_ALL}")
             else:
-                print(f"{player1_name if current_player == 'X' else player2_name} wins!")
+                print(f"{player1_name if current_player == 'X'else player2_name} wins!")
             break
 
         if check_for_tie(board):
